3571545482c9b56cf20b63a79835d0caedc1b363
gilberto-009199 <gilberto.tec@vivaldi.net>
2018-09-12 11:13:24 -0300

    Deixando o caregamento de livros dinamico

--- app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
+++ app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
@@ -2,16 +2,69 @@ package br.com.senaijandira.mybooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import br.com.senaijandira.mybooks.model.Livro;
+import br.com.senaijandira.mybooks.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout listaLivro;
+    public static Livro[] livros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listaLivro = findViewById(R.id.listalivros);
 
+        //cria livros
+
+        livros = new Livro[]{
+                new Livro(1,Utils.toByteArray(getResources(),R.drawable.pequeno_principe),
+                        "oi",getString(R.string.PeguenoPrincipe)),
+                new Livro(1,Utils.toByteArray(getResources(),R.drawable.pequeno_principe),
+                        "oi",getString(R.string.PeguenoPrincipe))
+        };
+
+
+        for(Livro l :livros){
+            addLivro(l,listaLivro);
+        }
+        /*
+        Livro livroTmp = new Livro();
+
+        byte[] capa = Utils.toByteArray(getResources(), R.drawable.pequeno_principe);
+
+        livroTmp.setId(1);
+        livroTmp.setCapa(capa);
+        livroTmp.setTitulo("Pequeno Principe");
+        livroTmp.setDescricao("Descrição do pequeno princpe:");
+
+        addLivro(livroTmp,listaLivro);*/
+    }
+    private void addLivro(Livro livro, ViewGroup root){
+                //carrega o layout do arquivo livro_layuot passado o aonde estará root e dizendo que o objeto não herdadra atruibutos do root
+        View v = LayoutInflater.from(this).inflate(R.layout.livro_layout,root,false);
+
+        ImageView capa = v.findViewById(R.id.imgLivroCapa);// pega o elemento dentro da v
+
+        TextView titulo = v.findViewById(R.id.txtLivroTitulo);
+
+        TextView descricao = v.findViewById(R.id.txtLivroDescricao);
+
+        capa.setImageBitmap(Utils.toBitmap(livro.getCapa()));
+
+        titulo.setText(livro.getTitulo());
+
+        descricao.setText(livro.getDescricao());
+
+        root.addView(v);
     }
 }
--- app/src/main/java/br/com/senaijandira/mybooks/model/Livro.java
+++ app/src/main/java/br/com/senaijandira/mybooks/model/Livro.java
@@ -1,49 +1,49 @@
-package br.com.senaijandira.mybooks.model;
+    package br.com.senaijandira.mybooks.model;
 
-public class Livro {
-    private int id;
-    private byte[] capa;
-    private String titulo;
-    private String descricao;
+    public class Livro {
+        private int id;
+        private byte[] capa;
+        private String titulo;
+        private String descricao;
 
-    public void Livro(){
+        public  Livro(){
 
-    }
-    public void Livro(int id, byte[] capa, String titulo,String descricao){
-    this.id= id;
-    this.capa = capa;
-    this.titulo = titulo;
-    this.descricao =descricao;
-    }
-    public int getId() {
-        return id;
-    }
+        }
+        public  Livro(int id, byte[] capa, String titulo,String descricao){
+            this.id= id;
+            this.capa = capa;
+            this.titulo = titulo;
+            this.descricao =descricao;
+        }
+        public int getId() {
+            return id;
+        }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+        public void setId(int id) {
+            this.id = id;
+        }
 
-    public byte[] getCapa() {
-        return capa;
-    }
+        public byte[] getCapa() {
+            return capa;
+        }
 
-    public void setCapa(byte[] capa) {
-        this.capa = capa;
-    }
+        public void setCapa(byte[] capa) {
+            this.capa = capa;
+        }
 
-    public String getTitulo() {
-        return titulo;
-    }
+        public String getTitulo() {
+            return titulo;
+        }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
 
-    public String getDescricao() {
-        return descricao;
-    }
+        public String getDescricao() {
+            return descricao;
+        }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
     }
-}
